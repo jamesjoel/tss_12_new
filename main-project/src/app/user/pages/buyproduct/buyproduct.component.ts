8e7bdf6f8725685d5e15459b714c0c8d475e7689
jamesjoel <jamesjoelj@gmail.com>
2022-11-28 13:13:40 +0530

    slider

--- main-project/src/app/user/pages/buyproduct/buyproduct.component.ts
+++ main-project/src/app/user/pages/buyproduct/buyproduct.component.ts
@@ -10,21 +10,47 @@ import { ProductService } from 'src/app/services/product.service';
 export class BuyproductComponent implements OnInit {
 
   id:any;
-  product:any;
+  product:any={
+    img1 : "gallery3.jpg",
+    img2 : "gallery1.jpg",
+    img3 : "gallery2.jpg",
+    img4 : "gallery2.png"
+
+  };
+
+  mainImg:any = this.product.img1;
+  bigImg = false;
+  
+
+
+
+
   constructor(
     private _actRoute : ActivatedRoute,
     private _product : ProductService
   ) {
-    this.id = this._actRoute.snapshot.paramMap.get("id");
-    // console.log(this.id);
-    this._product.getOneProduct(this.id).subscribe(result=>{
-      this.product= result;
-      console.log(result);
-    })
+    // this.id = this._actRoute.snapshot.paramMap.get("id");
+    // // console.log(this.id);
+    // this._product.getOneProduct(this.id).subscribe(result=>{
+    //   this.product= result;
+    //   console.log(result);
+    // })
 
    }
 
   ngOnInit(): void {
   }
 
+  changeMainImg(a:any){
+    this.mainImg = a;
+  }
+
+
+  showBigImg(){
+      this.bigImg = true;
+    }
+    hideBigImg(){
+    this.bigImg = false;
+
+  }
 }
